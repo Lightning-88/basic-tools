8b7fb73ddc4691712d182da1847705959bbfbde5
lightning-88 <erlanggasatria6677@gmail.com>
2025-12-09 23:37:04 +0700

    added decrypt function

--- src/app/xor-cipher/page.jsx
+++ src/app/xor-cipher/page.jsx
@@ -3,7 +3,8 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { xorCipher } from "@/lib/XORCipher";
+import { Separator } from "@/components/ui/separator";
+import { xorCipher, xorDecipher } from "@/lib/XORCipher";
 import { useState } from "react";
 
 export default function XORChipherPage() {
@@ -11,64 +12,118 @@ export default function XORChipherPage() {
   const [key, setKey] = useState("");
   const [salts, setSalts] = useState(16);
   const [encryptText, setEncryptText] = useState("");
+  const [encryptedText, setEncryptedText] = useState("");
+  const [decryptKey, setDecryptKey] = useState("");
+  const [resultDecrypt, setResultDecrypt] = useState("");
 
   const handleEncrypt = () => {
     if (!key || !plainText) return alert("kosong");
     if (salts <= 0 || salts > 20) return alert("salt 1-20");
     if (isNaN(salts)) return alert("harus angka 1-20");
 
-    const { output, salt } = xorCipher(plainText, key, parseInt(salts));
+    const output = xorCipher(plainText, key, parseInt(salts));
     setEncryptText(output);
   };
 
+  const handleDecrypt = () => {
+    const output = xorDecipher(encryptedText, decryptKey);
+    setResultDecrypt(output);
+  };
+
   return (
-    <div className="flex justify-center items-center py-4">
-      <div className="flex flex-col space-y-6 p-8 border border-zinc-200 rounded-2xl mx-4 max-w-sm w-full dark:border-zinc-700">
-        <h1 className="text-center text-2xl font-bold">XOR Cipher</h1>
+    <div className="flex justify-center items-center py-8">
+      <div className="p-8 space-y-16 border border-zinc-200 rounded-2xl mx-4 max-w-sm w-full dark:border-zinc-700">
+        <div className="flex flex-col space-y-6">
+          <h1 className="text-center text-2xl font-bold">XOR Cipher</h1>
 
-        <div className="grid w-full max-w-sm items-center gap-3">
-          <Label htmlFor="plainText">Plain Text</Label>
-          <Input
-            type="text"
-            id="plainText"
-            placeholder="Plain Text"
-            onChange={(e) => setPlainText(e.target.value)}
-            required
-          />
-        </div>
-
-        <div className="grid w-full max-w-sm items-center gap-3">
-          <Label htmlFor="salt">Salt</Label>
-          <Input
-            type="text"
-            inputMode="numeric"
-            id="salt"
-            placeholder="1-20 (Default 16)"
-            onChange={(e) => setSalts(e.target.value)}
-            value={salts}
-          />
-        </div>
-
-        <div className="grid w-full max-w-sm items-center gap-3">
-          <Label htmlFor="key">Key</Label>
-          <Input
-            id="key"
-            type="password"
-            placeholder="Create Strong Key"
-            onChange={(e) => setKey(e.target.value)}
-            required
-          />
-        </div>
-
-        <Button className="cursor-pointer" onClick={handleEncrypt}>
-          Encrypt
-        </Button>
-
-        {encryptText && (
-          <div>
-            <h1>{encryptText}</h1>
+          <div className="grid w-full max-w-sm items-center gap-3">
+            <Label htmlFor="plainText">Plain Text</Label>
+            <Input
+              type="text"
+              id="plainText"
+              placeholder="Plain Text"
+              onChange={(e) => setPlainText(e.target.value)}
+              required
+            />
           </div>
-        )}
+
+          <div className="grid w-full max-w-sm items-center gap-3">
+            <Label htmlFor="salt">Salt</Label>
+            <Input
+              type="text"
+              inputMode="numeric"
+              id="salt"
+              placeholder="1-20 (Default 16)"
+              onChange={(e) => setSalts(e.target.value)}
+              value={salts}
+            />
+          </div>
+
+          <div className="grid w-full max-w-sm items-center gap-3">
+            <Label htmlFor="key">Key</Label>
+            <Input
+              id="key"
+              type="password"
+              placeholder="Create Strong Key"
+              onChange={(e) => setKey(e.target.value)}
+              required
+            />
+          </div>
+
+          <Button className="cursor-pointer" onClick={handleEncrypt}>
+            Encrypt
+          </Button>
+
+          {encryptText && (
+            <div>
+              <textarea
+                defaultValue={encryptText}
+                className="w-full"
+              ></textarea>
+            </div>
+          )}
+        </div>
+
+        <Separator />
+
+        <div className="flex flex-col space-y-6">
+          <h1 className="text-center text-2xl font-bold">Decrypt</h1>
+
+          <div className="grid w-full max-w-sm items-center gap-3">
+            <Label htmlFor="encryptedText">Encrypted Text</Label>
+            <Input
+              type="text"
+              id="encryptedText"
+              placeholder="Encrypted Text"
+              onChange={(e) => setEncryptedText(e.target.value)}
+              required
+            />
+          </div>
+
+          <div className="grid w-full max-w-sm items-center gap-3">
+            <Label htmlFor="decryptKey">Key</Label>
+            <Input
+              id="decryptKey"
+              type="password"
+              placeholder="Enter Key"
+              onChange={(e) => setDecryptKey(e.target.value)}
+              required
+            />
+          </div>
+
+          <Button className="cursor-pointer" onClick={handleDecrypt}>
+            Decrypt
+          </Button>
+
+          {decryptKey && (
+            <div>
+              <textarea
+                defaultValue={resultDecrypt}
+                className="w-full"
+              ></textarea>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/lib/XORCipher.js
+++ src/lib/XORCipher.js
@@ -5,26 +5,40 @@ function generateSalt(size = 16) {
 }
 
 function hashKey(key, salt) {
-  const hashed = crypto
+  return crypto
     .createHash("sha256")
     .update(key + salt)
     .digest();
-
-  return [hashed, key];
 }
 
 export function xorCipher(text, key, size) {
-  const resultSalt = generateSalt(size);
-  const [hashedKey, salt] = hashKey(key, resultSalt);
+  const salt = generateSalt(size);
+  const hashedKey = hashKey(key, salt);
 
   const textBuffer = Buffer.from(text, "utf-8");
+  const saltB64 = Buffer.from(salt);
   const outBuffer = Buffer.alloc(textBuffer.length);
 
   for (let i = 0; i < outBuffer.length; i++) {
     outBuffer[i] = textBuffer[i] ^ hashedKey[i % hashedKey.length];
   }
 
-  const output = outBuffer.toString("base64");
+  return `${saltB64.toString("base64")}.${outBuffer.toString("base64")}`;
+}
 
-  return { output, salt };
+export function xorDecipher(combined, key) {
+  const [saltB64, cipherB64] = combined.split(".");
+  console.log(combined);
+
+  const salt = Buffer.from(saltB64, "base64").toString("utf8");
+  const hashedKey = hashKey(key, salt);
+
+  const dataBuffer = Buffer.from(cipherB64, "base64");
+  const outBuffer = Buffer.alloc(dataBuffer.length);
+
+  for (let i = 0; i < dataBuffer.length; i++) {
+    outBuffer[i] = dataBuffer[i] ^ hashedKey[i % hashedKey.length];
+  }
+
+  return outBuffer.toString("utf8");
 }
