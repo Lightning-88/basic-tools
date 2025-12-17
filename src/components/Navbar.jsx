c1f207145bbe8838f22c101227fcb3b8a3fb3e21
lightning-88 <erlanggasatria6677@gmail.com>
2025-12-17 22:07:56 +0700

    add responsive for mobile device

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,22 @@
 "use client";
 
-import { ChevronDown, ChevronUp } from "lucide-react";
+import { ChevronDown, ChevronUp, Menu, X } from "lucide-react";
 import Link from "next/link";
-import { useState } from "react";
+import { usePathname } from "next/navigation";
+import { useEffect, useState } from "react";
 
 export default function Navbar() {
   const [isMouseOver, setIsMouseOver] = useState(false);
+  const [isOpen, setIsOpen] = useState(false);
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (isOpen) setIsOpen(false);
+  }, [pathname]);
 
   return (
-    <header>
-      <nav className="flex h-14 items-center justify-between gap-8 px-4 sm:px-6 border-b border-zinc-200 dark:border-zinc-700">
+    <header className="fixed shadow-lg z-10 shadow-shadow-black h-14 px-4 bg-zinc-50 dark:bg-zinc-950 w-full">
+      <nav className="flex items-center justify-between gap-8 h-full border-b border-zinc-200 dark:border-zinc-700">
         <div>
           <Link href="/">
             <h1 className="font-semibold">Basic Tools</h1>
@@ -49,46 +56,66 @@ export default function Navbar() {
           <Link href="/" className="text-gray-950 text-sm/6 dark:text-white">
             Showcase
           </Link>
-          <Link
-            href="/"
-            className="group relative px-1.5 text-sm/6 text-sky-800 dark:text-sky-300"
-          >
-            <span className="absolute inset-0 border border-dashed border-sky-300/60 bg-sky-400/10 group-hover:bg-sky-400/15 dark:border-sky-300/30"></span>
-            Plus
-            <svg
-              width="5"
-              height="5"
-              viewBox="0 0 5 5"
-              className="absolute top-[-2px] left-[-2px] fill-sky-300 dark:fill-sky-300/50"
-            >
-              <path d="M2 0h1v2h2v1h-2v2h-1v-2h-2v-1h2z"></path>
-            </svg>
-            <svg
-              width="5"
-              height="5"
-              viewBox="0 0 5 5"
-              className="absolute top-[-2px] right-[-2px] fill-sky-300 dark:fill-sky-300/50"
-            >
-              <path d="M2 0h1v2h2v1h-2v2h-1v-2h-2v-1h2z"></path>
-            </svg>
-            <svg
-              width="5"
-              height="5"
-              viewBox="0 0 5 5"
-              className="absolute bottom-[-2px] left-[-2px] fill-sky-300 dark:fill-sky-300/50"
-            >
-              <path d="M2 0h1v2h2v1h-2v2h-1v-2h-2v-1h2z"></path>
-            </svg>
-            <svg
-              width="5"
-              height="5"
-              viewBox="0 0 5 5"
-              className="absolute right-[-2px] bottom-[-2px] fill-sky-300 dark:fill-sky-300/50"
-            >
-              <path d="M2 0h1v2h2v1h-2v2h-1v-2h-2v-1h2z"></path>
-            </svg>
-          </Link>
         </div>
+
+        <div
+          className={`border-l border-zinc-200 bg-zinc bg-zinc-50 h-screen fixed right-0 top-0 pt-4 px-4 w-64 ${
+            isOpen ? "translate-x-0" : "translate-x-full"
+          } z-10 transition-transform duration-200 md:hidden dark:bg-zinc-950 dark:border-zinc-700`}
+        >
+          <div className="flex justify-between mb-8">
+            <h1>Basic Tools</h1>
+            <button onClick={() => setIsOpen(false)}>
+              <X />
+            </button>
+          </div>
+
+          <div className="flex flex-col gap-2">
+            <Link href="/" className="text-gray-950 text-sm/6 dark:text-white">
+              Docs
+            </Link>
+            <div
+              className="relative inline-block"
+              onMouseEnter={() => setIsMouseOver(true)}
+              onMouseLeave={() => setIsMouseOver(false)}
+            >
+              <button className="flex gap-1 text-gray-950 text-sm/6 dark:text-white">
+                Tools
+                {isMouseOver ? (
+                  <ChevronUp width={12} aria-hidden="false" />
+                ) : (
+                  <ChevronDown width={12} />
+                )}
+              </button>
+              <span
+                className={`absolute ${
+                  isMouseOver ? "flex" : "hidden"
+                } text-sm flex-col text-left border top-6 gap-2 rounded-md bg-zinc-50 dark:bg-zinc-900 border-zinc-200 dark:border-zinc-700 left-0 p-4 w-44`}
+              >
+                <Link href="/qris-generator">Qris Generator</Link>
+                <Link href="/qrcode">QR Generator</Link>
+              </span>
+            </div>
+            <button className="flex gap-1 text-gray-950 text-sm/6 dark:text-white">
+              List
+              <ChevronDown width={12} />
+            </button>
+            <Link href="/" className="text-gray-950 text-sm/6 dark:text-white">
+              Showcase
+            </Link>
+          </div>
+        </div>
+
+        <button className="md:hidden" onClick={() => setIsOpen(true)}>
+          <Menu />
+        </button>
+
+        {isOpen && (
+          <div
+            className="fixed top-0 right-0 bottom-0 left-0 bg-[rgba(0,0,0,0.4)]"
+            onClick={() => setIsOpen(false)}
+          />
+        )}
       </nav>
     </header>
   );
