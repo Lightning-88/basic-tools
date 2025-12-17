f33ac58c4db458f5eb6981bac151e36cb5b511fb
lightning-88 <erlanggasatria6677@gmail.com>
2025-12-17 22:08:56 +0700

    adding change color feature

--- src/app/qrcode/page.jsx
+++ src/app/qrcode/page.jsx
@@ -11,6 +11,7 @@ import { v4 } from "uuid";
 export default function QRCode() {
   const [text, setText] = useState(null);
   const [resultURL, setResultURL] = useState(null);
+  const [color, setColor] = useState({ dark: "#000009", light: "#ffffff" });
   const canvasRef = useRef(null);
   const uuidv4 = v4();
 
@@ -18,36 +19,86 @@ export default function QRCode() {
     const canvas = canvasRef.current;
 
     qrcode
-      .toDataURL(canvas, text, { margin: 4, width: 400, height: 400 })
+      .toDataURL(canvas, text, {
+        margin: 4,
+        width: 400,
+        height: 400,
+        color: {
+          dark: color.dark,
+          light: color.light,
+        },
+      })
       .then((result) => {
         setResultURL(result);
       })
       .catch((e) => {
         console.log(e.message);
       });
-  }, [text]);
+  }, [text, color]);
 
   return (
-    <div className="flex justify-center items-center py-4 flex-col md:flex-row">
+    <div className="flex justify-center items-center py-4 flex-col gap-4 md:flex-row">
       <div className="flex flex-col border border-zinc-200 rounded-2xl mx-4 dark:border-zinc-700">
         {!text ? (
-          <QrCode width={400} height={400} className="opacity-50" />
+          <QrCode className="opacity-50 h-[400px] w-[400px] max-w-full max-h-[24rem] sm:max-h-full" />
         ) : (
           <div className="max-w">
-            <canvas ref={canvasRef} width={400} height={400}></canvas>
+            <canvas
+              ref={canvasRef}
+              className="rounded-2xl max-w-full max-h-[24rem] sm:max-h-full"
+            ></canvas>
           </div>
         )}
       </div>
 
       <div className="flex flex-col space-y-6 p-8 border border-zinc-200 rounded-2xl mx-4 dark:border-zinc-700">
         <div className="grid w-full max-w-sm items-center gap-3">
-          <Label htmlFor="text">Masukkan Text</Label>
+          <Label htmlFor="text">Masukkan Teks</Label>
           <Input
             type="text"
             id="text"
-            placeholder="Text"
+            placeholder="Abc..."
             onChange={(e) => setText(e.target.value)}
           />
+          <div className="space-y-2">
+            <h1>Atur Warna</h1>
+            <div className="space-y-2">
+              <div className="flex justify-between">
+                <Label htmlFor="background">Background</Label>
+                <Input
+                  className="w-12"
+                  type="color"
+                  id="background"
+                  onChange={(e) =>
+                    setColor((prev) => {
+                      return {
+                        ...prev,
+                        dark: e.target.value,
+                      };
+                    })
+                  }
+                  value={color.dark}
+                />
+              </div>
+              <div className="flex justify-between">
+                <Label htmlFor="color">Warna</Label>
+                <Input
+                  className="w-12"
+                  type="color"
+                  id="color"
+                  onChange={(e) =>
+                    setColor((prev) => {
+                      return {
+                        ...prev,
+                        light: e.target.value,
+                      };
+                    })
+                  }
+                  value={color.light}
+                />
+              </div>
+            </div>
+          </div>
         </div>
 
         <Button
